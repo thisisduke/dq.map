f57a4b656cfd9d24f0868b762f11dae6b54ac86d
thisisduke <835970331@qq.com>
2022-02-28 14:34:32 +0800

    remove

--- src/api/types/basic.ts
+++
@@ -1,6 +0,0 @@
-export interface AuthLoginType {
-    account: string;
-    password: string;
-    captchaCode: string;
-    captchaCodeToken: string;
-}
--- src/store/interface.ts
+++
@@ -1,8 +0,0 @@
-// import UserStateTypes from '@/store/modules/user/interface'
-export default interface RootStateTypes {
-  email:string
-}
-
-// export interface AllStateTypes extends RootStateTypes {
-//   userModules:UserStateTypes
-// }
